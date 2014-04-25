b2c2f356d5dce3020f2f8bcac71e7c6d37031b0f
hesta <007hei@gmail.com>
2014-04-25 14:32:55 -0500

    initial commit

---
+++ linear_regression/example_sbt_configuration.sbt
@@ -0,0 +1,12 @@
+name := "LR"
+
+version := "1.0"
+
+scalaVersion := "2.10.3"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "0.9.1"
+
+libraryDependencies += "org.apache.spark" %% "spark-mllib" % "0.9.1"
+
+resolvers += "Akka Repository" at "http://repo.akka.io/releases/"
+
---
+++ linear_regression/stock_forecast_trend_model.scala
@@ -0,0 +1,39 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.mllib.regression.LinearRegressionWithSGD
+import org.apache.spark.mllib.regression.LabeledPoint
+
+object Linear_regression{
+
+def main(args:Array[String]){
+
+    val sc = new SparkContext("local", "LR", System.getenv("SPARK_HOME"), SparkContext.jarOfClass(this.getClass))
+
+        // Load and parse the data
+    val data = sc.textFile("train")
+    val parsedData = data.map { line =>
+          val parts = line.split(',')
+          LabeledPoint(parts(0).toDouble, parts(1).split(',').map(x => x.toDouble).toArray)
+    }
+
+    val data1 = sc.textFile("test")
+    val parsedData1 = data1.map { line =>
+            val parts1 = line.split(',')
+            LabeledPoint(parts1(0).toDouble, parts1(1).split(',').map(x => x.toDouble).toArray)
+    }
+
+    // Building the model
+    val numIterations = 1000
+    val model = LinearRegressionWithSGD.train(parsedData, numIterations)
+
+    // Evaluate model on training examples and compute training error
+    val valuesAndPreds = parsedData1.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+
+    val error_rate = valuesAndPreds.map{ case(v, p) => math.abs(v - p)}.reduce(_ + _)/valuesAndPreds.count
+    println("Training Error = " + error_rate)
+ }
+
+}
